e0f35f05dc14c2e3fec5994fb1ac2d98a797cee4
weidajiangjiu <549998272@qq.com>
2020-01-04 15:37:19 +0800

    订单管理更新

--- src/pages/comment/List.vue
+++ src/pages/comment/List.vue
@@ -130,7 +130,24 @@ export default {
             comments:[],
             form:{
               type:"comment"
-            }
+            },
+             options: [{
+          value: '选项1',
+          label: '黄金糕'
+        }, {
+          value: '选项2',
+          label: '双皮奶'
+        }, {
+          value: '选项3',
+          label: '蚵仔煎'
+        }, {
+          value: '选项4',
+          label: '龙须面'
+        }, {
+          value: '选项5',
+          label: '北京烤鸭'
+        }],
+
         } 
     },
     created (){
